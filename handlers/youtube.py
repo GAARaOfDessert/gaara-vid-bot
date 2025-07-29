fdeb22cb26f0697bb50785afe8077085904a5ad8
GAARaOfDessert <154476404+GAARaOfDessert@users.noreply.github.com>
2025-07-29 13:41:07 +0100

    Update youtube.py

--- handlers/youtube.py
+++ handlers/youtube.py
@@ -12,6 +12,7 @@ async def download_video(update, context, url):
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
             video_path = ydl.prepare_filename(info)
+            cmd = ["bin/yt-dlp", "--cookies", "cookies.txt", "-f", "mp4", "-o", f"{filename}.mp4, url]
 
         await update.message.reply_video(video=open(video_path, 'rb'))
         os.remove(video_path)
